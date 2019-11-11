5f1884831ae2b32181f3dbbd0e5e0e6f40ad8420
Yuri-Tiofilo <yusilva12345@gmail.com>
2019-11-11 10:05:14 -0300

    'createComponentHeader'

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,18 +1,30 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import {Container, Text} from './styles';
+import {Container, Text, AreaIcon, Icon} from './styles';
 
-export default function Header({title}) {
+export default function Header({title, icoName, icoSize, colorIcon}) {
   return (
     <Container>
+      <AreaIcon>
+        <Icon name={icoName} size={icoSize} colorIcon={colorIcon} />
+      </AreaIcon>
       <Text>{title}</Text>
+      <AreaIcon>
+        <Icon name={icoName} size={icoSize} colorIcon={colorIcon} />
+      </AreaIcon>
     </Container>
   );
 }
 
 Header.propTypes = {
   title: PropTypes.string,
+  icoName: PropTypes.string,
+  icoSize: PropTypes.number,
+  colorIcon: PropTypes.string,
 };
 Header.defaultProps = {
   title: 'undefield',
+  icoName: 'fonticons',
+  icoSize: 22,
+  colorIcon: '#eeee',
 };
--- src/components/Header/styles.js
+++ src/components/Header/styles.js
@@ -1,7 +1,8 @@
 import styled from 'styled-components/native';
+import Icons from 'react-native-vector-icons/FontAwesome';
 
 export const Container = styled.View`
-  flex-direction: column;
+  flex-direction: row;
   background: #3f51b5;
   height: 80px;
   justify-content: flex-start;
@@ -11,4 +12,16 @@ export const Text = styled.Text`
   color: #fefaf4;
   font-size: 16px;
   font-weight: bold;
+  align-content: center;
+`;
+export const AreaIcon = styled.TouchableOpacity`
+  flex: 1;
+  align-items: center;
+  justify-content: center;
+`;
+
+export const Icon = styled(Icons)`
+  font-size: 22px;
+  font-weight: bold;
+  color: #eee;
 `;
