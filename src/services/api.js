a26f46cd6701db77ae08f908dd72a286ef5a920d
Yuri-Tiofilo <yusilva12345@gmail.com>
2020-02-04 10:40:39 -0300

    Change Service API

--- src/services/api.js
+++ src/services/api.js
@@ -3,6 +3,7 @@ import axios from 'axios';
 // utiliza adb reverse tcp:3333 tcp:3333
 const api = axios.create({
   baseURL: 'https://node-nova-dublagem.herokuapp.com',
+  // baseURL: 'http://novadublagemweb.com.br',
 });
 
 export default api;
